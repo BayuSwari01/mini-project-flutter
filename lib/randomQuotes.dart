ed74d42160ea76b0802dbc0157c1cb8cf0cf61f8
BayuSwari01 <bayuswari.01@gmail.com>
2023-01-23 21:52:02 +0700

    connect to API

--- lib/randomQuotes.dart
+++ lib/randomQuotes.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class randomQuotes extends StatefulWidget {
@@ -8,8 +10,39 @@ class randomQuotes extends StatefulWidget {
 }
 
 class _randomQuotesState extends State<randomQuotes> {
+  String? quotes, author;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getQuote();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Random"),);
+    return Container(
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            quotes ?? 'Wait...',
+            textAlign: TextAlign.center,
+          ),
+          Text('- ' + (author ?? 'Wait...')),
+        ],
+      )),
+    );
+  }
+
+  void _getQuote() async {
+    var res = await http.get(Uri.parse("https://api.quotable.io/random"));
+    var body = jsonDecode(res.body);
+
+    setState(() {
+      quotes = body['content'];
+      author = body['author'];
+    });
   }
 }
--- lib/randomQuotesTechnology.dart
+++ lib/randomQuotesTechnology.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class randomQuotesTechnology extends StatefulWidget {
@@ -8,8 +10,40 @@ class randomQuotesTechnology extends StatefulWidget {
 }
 
 class _randomQuotesTechnologyState extends State<randomQuotesTechnology> {
+  String? quotes, author;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getQuote();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Random Technology"),);
+    return Container(
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            quotes ?? 'Wait...',
+            textAlign: TextAlign.center,
+          ),
+          Text('- ' + (author ?? 'Wait...')),
+        ],
+      )),
+    );
+  }
+
+  void _getQuote() async {
+    var res = await http
+        .get(Uri.parse("https://api.quotable.io/random?tags=technology"));
+    var body = jsonDecode(res.body);
+
+    setState(() {
+      quotes = body['content'];
+      author = body['author'];
+    });
   }
 }
